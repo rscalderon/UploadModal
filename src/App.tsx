6101b2a464cc1f4a1c4d0d38cc11a4e4842fd6a3
rscalderon <samourcalderon@gmail.com>
2024-01-09 18:49:20 -0500

    initialize React app and file structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Modal from './components/Modal';
+
+// App currently only renders the upload document Modal, but in a production application would also contain the rest of the React app
+function App() {
+  return (
+    <>
+      <Modal />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Modal.tsx
@@ -0,0 +1,27 @@
+import reactLogo from '../assets/react.svg';
+import viteLogo from '/vite.svg';
+
+import './Modal.css';
+
+function Modal() {
+  return (
+    <div id='upload-modal'>
+      <h1>Document Upload</h1>
+      <div>
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} className='logo' alt='Vite logo' />
+        </a>
+        <a href='https://react.dev' target='_blank'>
+          <img src={reactLogo} className='logo react' alt='React logo' />
+        </a>
+      </div>
+      <p>
+        Data in the import file is correct. Please press Continue to import.
+      </p>
+      <button>Continue Import</button>
+      <button>Cancel</button>
+    </div>
+  );
+}
+
+export default Modal;
