03a5b4f721b2e26a88724041981c17ffc2a1a4bf
rscalderon <samourcalderon@gmail.com>
2024-01-10 14:54:02 -0500

    added document upload component, styled borders throughout and rearranged buttons

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,30 +1,42 @@
 import './Modal.css';
+import { FileUploader } from 'react-drag-drop-files';
 
 function Modal() {
   return (
     <div id='upload-modal'>
-      <div id='modal-header'>
-        <button className='close-button'>X</button>
-        <h1>Document Upload</h1>
-        <hr />
-      </div>
+      {/* Modal header */}
+      <header id='modal-header'>
+        <button id='close-button'>X</button>
+        <div id='header-container'>
+          <h1>Document Upload</h1>
+          <hr />
+        </div>
+      </header>
       {/* Modal body */}
       <div id='modal-content'>
-        <div id='left-column'>
+        <section id='left-column'>
           {/* <h2>LEFT</h2> */}
-          <label htmlFor='importName'>Select Import Name:</label>
-          <select id='importName'></select>
+          <div id='importName'>
+            <label htmlFor='importName'>Select Import Name:</label>
+            <select></select>
+          </div>
           <hr />
           <p>Select a manifest you'd like to import</p>
-          <h1>INSERT DOC UPLOAD COMPONENT HERE</h1>
+          <div id='File-upload-container'>
+            <FileUploader
+              id='FileUploader'
+              label='Drag & Drop Here Or Browse'
+            />
+            <button>Upload Manifest</button>
+          </div>
           <hr />
           <p>Elapse Data Checking</p>
-          <p>No Elapsed Dates!</p>
+          <p className='green'>No Elapsed Dates!</p>
           <hr />
           <p>Tolerance Window:</p>
           <p>Toggle ON/OFF | Select Tolerance Level</p>
-        </div>
-        <div id='right-column'>
+        </section>
+        <section id='right-column'>
           {/* <h2>RIGHT</h2> */}
           <div className='form-group'>
             <label>Split schedule using social distancing?</label>
@@ -40,9 +52,9 @@ function Modal() {
               <label htmlFor='no'>No</label>
               <hr />
               <p>Location Checking:</p>
-              <p>All Available!</p>
+              <p className='green'>All Available!</p>
               <hr />
-              <div className='client-form'>
+              <div id='client-form'>
                 <label>Client:</label>
                 <div className='radio-buttons'>
                   <input
@@ -65,31 +77,35 @@ function Modal() {
                     name='socialDistancing'
                     value='Multiple'
                   />
-                  <br />
-                  <label htmlFor='TestingCenter1'>TestingCenter1</label>
-                  <select id='TestingCenter1'></select>
-                  <br />
-                  <label htmlFor='TestingCenter2'>TestingCenter2</label>
-                  <select id='TestingCenter2'></select>
-                  <br />
-                  <label htmlFor='TestingCenter3'>TestingCenter3</label>
-                  <select id='TestingCenter3'></select>
-                  <br />
-                  <label htmlFor='TestingCenter4'>TestingCenter4</label>
-                  <select id='TestingCenter4'></select>
+                  <div className='Testing-Center'>
+                    <label htmlFor='TestingCenter1'>TestingCenter1</label>
+                    <select id='TestingCenter1'></select>
+                  </div>
+                  <div className='Testing-Center'>
+                    <label htmlFor='TestingCenter2'>TestingCenter2</label>
+                    <select id='TestingCenter2'></select>
+                  </div>
+                  <div className='Testing-Center'>
+                    <label htmlFor='TestingCenter3'>TestingCenter3</label>
+                    <select id='TestingCenter3'></select>
+                  </div>
+                  <div className='Testing-Center'>
+                    <label htmlFor='TestingCenter4'>TestingCenter4</label>
+                    <select id='TestingCenter4'></select>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </section>
       </div>
-      <div id='modal-footer'>
+      <footer id='modal-footer'>
         <p>
           Data in the import file is correct. Please press Continue to import.
         </p>
         <button>Continue Import</button>
-        <button>Cancel</button>
-      </div>
+        <button id='cancel-button'>Cancel</button>
+      </footer>
     </div>
   );
 }
