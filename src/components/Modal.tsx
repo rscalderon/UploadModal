0dd1c50584bf03e85f04e7a1dbd64c00a740d59b
rscalderon <samourcalderon@gmail.com>
2024-01-09 19:30:15 -0500

    removed unnecessary styles and assets, added hover styles and horizontal lines, cleaned up JSX in Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,6 +1,3 @@
-import reactLogo from '../assets/react.svg';
-import viteLogo from '/vite.svg';
-
 import './Modal.css';
 
 function Modal() {
@@ -8,14 +5,6 @@ function Modal() {
     <div id='upload-modal'>
       <h1>Document Upload</h1>
       <hr />
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
       <p>
         Data in the import file is correct. Please press Continue to import.
       </p>
